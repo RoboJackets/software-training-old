51efe68260714c2794fd531035355a9892a46ee7
jgibbygibs <jgibson37@gatech.edu>
2017-07-03 19:10:36 -0400

    made code python3 compliant

--- week1/week1.py
+++ week1/week1.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+
 ##########Global Variables##########
 
 ###########Primitive Data Types##########
-#python isn't a typed language like C++ or Java
-#This is a comment!
+# python is a typed language like C++ and Java
+# BUT... it is dynamically typed instead of statically typed like the other two
+# this means that at compile time the C++ and Java compliler knows what type variable is
+# but in python they do not
+# This is a comment!
 
 '''
 This is a block comment!
@@ -19,7 +24,7 @@ isRoboJacketsCool = True #boolean
 gpas = [1.0, 2.6, 4.0, 3.6]
 membersInRoboJackets = []
 
-def exampleFunction():
+def main():
 	#########Local Variables##########
 	number = 0
 	length = 0
@@ -29,30 +34,30 @@ def exampleFunction():
 	##########Operators#########
 	print (3 + 3 == 9)
 	print (2 + 2 != 5)
-	print not True
-	print True or False
-	print True and False
+	print (not True)
+	print (True or False)
+	print (True and False)
 
 	#########Loops#########
 	##If-ElseIf-Else
 	if (number == 0):
 		number = 3
-		print "I understand Loops!"
+		print ("I understand Loops!")
 	elif (number > 4):
 		number += 3
-		print "Like John Cena, you can't see me"
+		print ("Like John Cena, you can't see me")
 	else:
 		number -= 1
-		print "Like George P. Burdell, I don't ever show up"
+		print ("Like George P. Burdell, I don't ever show up")
 
 	##For
 	for n in range(len(gpas)):
-		print "GPA:", gpas[n]
+		print ("GPA:", gpas[n])
 
 	##While
 	length = 0
 	while (length < len(gpas)):
-		print "Adjusted GPA:", (gpas[length] + 2)
+		print ("Adjusted GPA:", (gpas[length] + 2))
 		length += 1
 
-exampleFunction()
+main()
