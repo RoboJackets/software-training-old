f500434f9d088c0d951b9fa2d207a11e36502163
Sarah Storer <sstorer3@gatech.edu>
2017-07-07 14:47:47 -0700

    Update week2.cpp

--- code/week2/week2.cpp
+++ code/week2/week2.cpp
@@ -1,1 +1,14 @@
+// make sure to compile with g++
+// to compile do
+//      g++ -o week2.out week2.cpp
+// to run do
+//      ./week2.out
 
+// This is including a header file for the standard library, strings, and input/output streaming
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+
+int main() {
+    return 0;
+}
