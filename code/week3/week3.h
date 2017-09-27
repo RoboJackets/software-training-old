6afcbcd883e96582b1e156f294279791854c5f52
Jason Gibson <jgibson37@gatech.edu>
2017-09-27 16:18:20 -0400

    reverted commits that removed week3 example code

---
+++ code/week3/week3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
