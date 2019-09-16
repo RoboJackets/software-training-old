a59e7a8ec5871ba8fb6a8678395d01a1b5a7ce26
Zach Olkin <zach.olkin@gmail.com>
2019-09-16 14:42:31 -0400

    Update projects/exercises/01/drive_in_line.cpp
    Co-Authored-By: Matthew Barulic <matthew.barulic@gmail.com>

--- projects/exercises/01/drive_in_line.cpp
+++ projects/exercises/01/drive_in_line.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    RJRobot robot(); //Initialize a RJ Robot
+    RJRobot robot; //Initialize a RJ Robot
     
     robot.setDriveMotors(1.0, 1.0); //Drive both robot motors forward at max speed
     robot.wait(2000ms); //Wait for 2000 milliseconds (2 seconds)
