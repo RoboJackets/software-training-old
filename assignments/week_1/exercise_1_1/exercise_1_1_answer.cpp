8dbe495a298e73c63070aa74cf7a807751f1bb7e
HussainGynai <43356296+HussainGynai@users.noreply.github.com>
2020-09-08 20:45:28 -0400

    Update exercise_1_1_answer.cpp
    Changed so the wheel speeds are fixed speeds instead of being incremented/decremented

--- assignments/week_1/exercise_1_1/exercise_1_1_answer.cpp
+++ assignments/week_1/exercise_1_1/exercise_1_1_answer.cpp
@@ -1,5 +1,4 @@
-//Do not edit the next 5 lines
-#include <stdlib.h>
+//Do not edit the next 4 lines
 #include <iostream>
 #include <vector>
 
@@ -22,25 +21,25 @@ int main(int argc, char** argv) {
         // Write rest of code here (within while loop)
         if (direction == 'w') {
             for (int i = 0; i < 4; i++) {
-                wheel_speeds[i]++; 
+                wheel_speeds[i] = 1; 
             }
         }
         else if (direction == 'a') {
-            wheel_speeds[0] -= 0.5;
-            wheel_speeds[2] -= 0.5;
-            wheel_speeds[1] += 1;
-            wheel_speeds[3] += 1;   
+            wheel_speeds[0] = 0.5;
+            wheel_speeds[2] = 0.5;
+            wheel_speeds[1] = 1;
+            wheel_speeds[3] = 1;   
         }
         else if (direction == 's') {
             for (int i = 0; i < 4; i++) {
-                wheel_speeds[i]--;
+                wheel_speeds[i] = -1;
             }
         }
         else if (direction == 'd') {
-            wheel_speeds[0] += 1;
-            wheel_speeds[2] += 1;
-            wheel_speeds[1] -= 0.5;
-            wheel_speeds[3] -= 0.5;
+            wheel_speeds[0] = 1;
+            wheel_speeds[2] = 1;
+            wheel_speeds[1] = 0.5;
+            wheel_speeds[3] = 0.5;
         }
         else if (direction == 'e') {
             for (int i = 0; i < 4; i++) {
