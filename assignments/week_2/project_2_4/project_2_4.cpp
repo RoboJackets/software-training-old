6e467da6e81b46b0f73227e5dcc95b1250d099a8
Matthew Barulic <matthew.barulic@gmail.com>
2020-09-10 18:44:18 -0700

    Makes project 2.4 file names match pattern of other exercises.

--- assignments/week_2/project_2_4/project_2_4.cpp
+++ assignments/week_2/project_2_4/project_2_4.cpp
@@ -1,6 +1,21 @@
 #include <iostream>  // Gives us access to std::cin and std::cout
 #include <string>  // Gives us access to std::string and std::getline()
 
+struct LightOutputs {
+  bool red_ns;
+  bool yellow_ns;
+  bool green_ns;
+  bool red_ew;
+  bool yellow_ew;
+  bool green_ew;
+};
+
+struct LightInputs {
+  int time;
+  bool car_sensor_n;
+  bool car_sensor_s;
+};
+
 // TODO Define your light controller state machine class here
 
 int main()
