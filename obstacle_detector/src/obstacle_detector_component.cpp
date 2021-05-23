fd8bc12890639aa923f615ea692074fec9fd6997
Matthew Barulic <matthew.barulic@gmail.com>
2021-05-23 18:26:29 -0400

    Got the rest of the node working.

--- obstacle_detector/src/obstacle_detector_component.cpp
+++ obstacle_detector/src/obstacle_detector_component.cpp
@@ -32,14 +32,18 @@ namespace obstacle_detector
 {
   void FindColors(const cv::Mat &input, const cv::Scalar &range_min, const cv::Scalar &range_max, cv::Mat &output)
   {
+    //// BEGIN STUDENT CODE
     cv::Mat input_hsv;
     cv::cvtColor(input, input_hsv, CV_BGR2HSV);
     cv::inRange(input_hsv, range_min, range_max, output);
+    //// END STUDENT CODE
   }
 
-  void ReprojectToGroundPlane(const cv::Mat &input, const cv::Mat &homography, cv::Mat &output)
+  void ReprojectToGroundPlane(const cv::Mat &input, const cv::Mat &homography, const cv::Size& map_size, cv::Mat &output)
   {
-    cv::warpPerspective(input, output, homography, input.size() /*TODO this size should be different*/);
+    //// BEGIN STUDENT CODE
+    cv::warpPerspective(input, output, homography, map_size, cv::INTER_NEAREST, cv::BORDER_CONSTANT, cv::Scalar(127));
+    //// END STUDENT CODE
   }
 
   class ObstacleDetectorComponent : public rclcpp::Node
@@ -62,12 +66,9 @@ namespace obstacle_detector
                                                        {"max.h", 0},
                                                        {"max.s", 0},
                                                        {"max.v", 0}});
-      cv::namedWindow("obstacles preview", cv::WINDOW_AUTOSIZE);
-    }
-
-    ~ObstacleDetectorComponent()
-    {
-      cv::destroyWindow("obstacles preview");
+      declare_parameter<double>("map_resolution", 100.0);  // px / m
+      declare_parameter<int>("map_width", 50);  // px
+      declare_parameter<int>("map_height", 100);  // px
     }
 
   private:
@@ -97,48 +98,80 @@ namespace obstacle_detector
         return;
       }
 
+      const auto map_resolution = get_parameter("map_resolution").as_double();
+      const auto map_size = cv::Size(get_parameter("map_width").as_int(), get_parameter("map_height").as_int());
+      cv::Mat map_camera_intrinsics;
+      cv::Mat map_camera_rotation;
+      cv::Mat map_camera_position;
+      GetMapCameraProperties(map_resolution, map_size, map_camera_intrinsics, map_camera_rotation, map_camera_position);
+
       const auto camera_matrix = cv::Mat(info_msg->k).reshape(1, 3);
 
-      const auto homography = GetHomography(camera_matrix, image_msg->header);
-
-      std::cout << homography << std::endl;
+      const auto homography = GetHomography(camera_matrix, image_msg->header, map_camera_intrinsics, map_camera_rotation, map_camera_position);
 
       cv::Mat projected_colors;
-      ReprojectToGroundPlane(detected_colors, homography, projected_colors);
+      ReprojectToGroundPlane(detected_colors, homography, map_size, projected_colors);
 
-      cv::imshow("obstacles preview", projected_colors);
-      cv::waitKey(10);
+      cv::flip(projected_colors, projected_colors, 0);
 
       nav_msgs::msg::OccupancyGrid occupancy_grid_msg;
       occupancy_grid_msg.header.stamp = image_msg->header.stamp;
-      occupancy_grid_msg.header.frame_id = "";
-      // TODO popuate message
+      occupancy_grid_msg.header.frame_id = "base_footprint";
+      occupancy_grid_msg.info.height = map_size.height;
+      occupancy_grid_msg.info.width = map_size.width;
+      occupancy_grid_msg.info.resolution = 1.0/map_resolution;
+      occupancy_grid_msg.info.map_load_time = image_msg->header.stamp;
+      occupancy_grid_msg.info.origin.position.x = 0;
+      occupancy_grid_msg.info.origin.position.y = map_size.width/(2.0 * map_resolution);
+      occupancy_grid_msg.info.origin.position.z = 0;
+      occupancy_grid_msg.info.origin.orientation.x = 0;
+      occupancy_grid_msg.info.origin.orientation.y = 0;
+      occupancy_grid_msg.info.origin.orientation.z = -0.7071068;
+      occupancy_grid_msg.info.origin.orientation.w = 0.7071068;
+      std::transform(projected_colors.begin<uint8_t>(), projected_colors.end<uint8_t>(), std::back_inserter(occupancy_grid_msg.data), [](const auto& pixel_value){
+        switch(pixel_value) {
+          case 0:
+            return 0;
+          case 255:
+            return 100;
+          case 127:
+          default:
+            return -1;
+        }
+      });
+
       occupancy_grid_publisher_->publish(occupancy_grid_msg);
     }
 
-    cv::Mat GetHomography(const cv::Mat& camera_matrix, const std_msgs::msg::Header& image_header)
+    void GetMapCameraProperties(const double map_resolution, const cv::Size& map_size, cv::Mat& intrinsics, cv::Mat& rotation, cv::Mat& position)
+    {
+      rotation = (cv::Mat_<double>(3,3) << 0, -1,  0, 
+                                          -1,  0,  0, 
+                                           0,  0, -1);
+      const auto z = 1.0;
+      const auto f = map_resolution * z;
+      const auto y = map_size.height / (2 * map_resolution);
+      position = (cv::Mat_<double>(3,1) << 0, y, z);
+      intrinsics = (cv::Mat_<double>(3,3) << f, 0, (map_size.width/2.0), 0, f, (map_size.height/2.0), 0, 0, 1);
+    }
+
+    cv::Mat GetHomography(const cv::Mat& camera_intrinsics, const std_msgs::msg::Header& image_header, const cv::Mat& map_camera_intrinsics, const cv::Mat& map_camera_rotation, const cv::Mat& map_camera_position)
     {
       const auto tf_transform = tf_buffer_.lookupTransform(image_header.frame_id, "base_footprint", image_header.stamp);
       const Eigen::Isometry3d eigen_transform = tf2::transformToEigen(tf_transform);
       cv::Mat opencv_transform;
       cv::eigen2cv(eigen_transform.matrix(), opencv_transform);
 
-      cv::Mat Ra = (cv::Mat_<double>(3,3) << 0, -1, 0, 
-                                            -1,  0, 0, 
-                                             0, 0, -1);
-      cv::Mat Ta = (cv::Mat_<double>(3,1) << 0, 0.75, 1.0);
-
       cv::Mat Rb = opencv_transform(cv::Range(0,3),cv::Range(0,3)).clone();
       cv::Mat Tb = opencv_transform(cv::Range(0,3),cv::Range(3,4)).clone();
 
-      cv::Mat n = cv::Mat::zeros(3,1,CV_64F);
-      n.at<double>(1,0) = -1.0;
+      // Assumes ground plane lies at Z=0 with a normal pointing towards +Z, in the base_footprint frame
+      cv::Mat n = Rb * (cv::Mat_<double>(3,1) << 0,0,1);
+      const auto d = cv::norm(Tb.mul(n));
 
-      const auto d = Tb.at<double>(1,0);
+      cv::Mat M = (map_camera_rotation*Rb.t()) - (((-map_camera_rotation*Rb.t()*Tb+map_camera_position)*n.t())/d);
 
-      cv::Mat M = (Ra*Rb.t()) - (((-Ra*Rb.t()*Tb+Ta)*n.t())/d);
-
-      cv::Mat H = camera_matrix * M * camera_matrix.inv();
+      cv::Mat H = map_camera_intrinsics * M * camera_intrinsics.inv();
 
       return H;
     }
