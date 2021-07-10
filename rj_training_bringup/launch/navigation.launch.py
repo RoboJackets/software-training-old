58570d2d4e7bf87d2267f11ab73d10b7498d1ee6
Matthew Barulic <matthew.barulic@gmail.com>
2021-07-10 00:15:37 -0400

    Adds mission_orchestrator to bringup. Fixes a lot of 'param' tags that should have been 'arg' tags.

--- rj_training_bringup/launch/navigation.launch.py
+++ rj_training_bringup/launch/navigation.launch.py
@@ -20,8 +20,7 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
