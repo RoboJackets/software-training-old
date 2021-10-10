3715e968a5c21f2d5d9cb8002def413b58771392
Matthew Barulic <matthew.barulic@gmail.com>
2021-10-10 16:14:18 -0400

    early thoughts about lqr

--- lqr_control/src/lqr_controller.cpp
+++ lqr_control/src/lqr_controller.cpp
@@ -1,8 +1,18 @@
 #include <nav2_core/controller.hpp>
 #include <pluginlib/class_list_macros.hpp>
+#include <tf2_eigen/tf2_eigen.h>
+#include <Eigen/Dense>
 
 namespace lqr_controller
 {
+
+Eigen::Vector3d StateFromMsg(const geometry_msgs::msg::PoseStamped& pose) {
+  Eigen::Quaterniond orientation;
+  tf2::fromMsg(pose.pose.orientation, orientation);
+  Eigen::Vector3d state;
+  state << pose.pose.position.x, pose.pose.position.y, orientation.toRotationMatrix().eulerAngles(0, 1, 2)[2];
+  return state;
+}
 
 class LqrController : public nav2_core::Controller
 {
@@ -19,12 +29,20 @@ public:
   void cleanup() override {}
 
   void setPlan(const nav_msgs::msg::Path & path) override {
-    global_path_ = path;
+    trajectory_.clear();
+    std::transform(path.poses.begin(), path.poses.end(), std::back_inserter(trajectory_), StateFromMsg);
   }
 
   geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped & pose, const geometry_msgs::msg::Twist & velocity) override
   {
-    
+    Eigen::Vector3d state = StateFromMsg(pose);
+
+    Eigen::Matrix3d Q;
+    Eigen::Matrix2d R;
+    Eigen::Matrix3d A;
+    Eigen::Matrix<double, 3, 2> B;
+
+
     geometry_msgs::msg::TwistStamped cmd_vel_msg;
     cmd_vel_msg.header.frame_id = "base_link";
     cmd_vel_msg.header.stamp = node_->now();
@@ -33,7 +51,7 @@ public:
 
 private:
   rclcpp_lifecycle::LifecycleNode::SharedPtr node_;
-  nav_msgs::msg::Path global_path_;
+  std::vector<Eigen::Vector3d> trajectory_;
 
 };
 
