a8ef1142e2aca03008b78471ca6c402b2c9d9420
Jason Gibson <jgibson37@gatech.edu>
2021-11-06 21:55:00 -0400

    commit for LQR based control of differential drive

--- lqr_control/src/lqr_controller.cpp
+++ lqr_control/src/lqr_controller.cpp
@@ -40,6 +40,12 @@ public:
   void configure(const rclcpp_lifecycle::LifecycleNode::SharedPtr & node, std::string name, const std::shared_ptr<tf2_ros::Buffer> & tf_buffer, const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap) override {
     node_ = node;
 
+    T_ = node->declare_parameter<double>("T", 1.0);
+    dt_ = node->declare_parameter<double>("dt", 0.1);
+
+    S_.resize(T_/dt_);
+    prev_u_.resize(T_/dt_);
+    prev_x_.resize(T_/dt_);
   }
 
   void activate() override {}
@@ -55,22 +61,89 @@ public:
 
   geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped & pose, const geometry_msgs::msg::Twist & velocity) override
   {
+    RCLCPP_INFO(node_->get_logger(), "Got request for control");
     Eigen::Vector3d state = StateFromMsg(pose);
 
-    Eigen::Matrix3d Q;
-    Eigen::Matrix2d R;
-    Eigen::Matrix3d A;
-    Eigen::Matrix<double, 3, 2> B;
-
+    computeRicattiEquation(state);
 
     geometry_msgs::msg::TwistStamped cmd_vel_msg;
+    cmd_vel_msg.twist.linear.x = prev_u_[0](0);
+    cmd_vel_msg.twist.angular.z = prev_u_[0](1);
     cmd_vel_msg.header.frame_id = "base_link";
     cmd_vel_msg.header.stamp = node_->now();
     return cmd_vel_msg;
   }
 
+  Eigen::Matrix3d computeAMatrix(const Eigen::Vector3d& x, const Eigen::Vector2d& u) {
+    return Eigen::Matrix3d::Identity();
+  }
+
+  Eigen::Matrix<double, 3, 2> computeBMatrix(const Eigen::Vector3d& x) {
+    Eigen::Matrix<double, 3, 2> B = Eigen::Matrix<double, 3, 2>::Zero();
+    B(0, 0) = cos(x(2)) * dt_;
+    B(1, 0) = sin(x(2)) * dt_;
+    B(2, 1) = dt_;
+    return B;
+  }
+
+  //double computeRunningCost(const Eigen::Vector3d x, const Eigen::Vector2d u) {
+  //  double state = x.transpose() * Q_ * x;
+  //  double control = u.transpose() * R_ * u;
+  //  return 0.5 * state + 0.5 * control;
+  //}
+
+  //double computeTerminalCost(const Eigen::Vector3d xf) {
+  //  return 0.5 * xf.transpose() * Qf_ * xf;
+  //}
+
+  Eigen::Vector3d computeNextState(const Eigen::Vector3d x, const Eigen::Vector2d u) {
+    return computeAMatrix(x, u) * x + computeBMatrix(x) * u;
+  }
+
+  void computeRicattiEquation(const Eigen::Vector3d& init_x) {
+    // does the backwards pass of the ricatti equation
+    S_[S_.size()-1] = Qf_;
+    for(int t = T_/dt_ - 1; t > 0; t--) {
+      auto last_S = S_[t+1];
+      Eigen::Matrix3d A = computeAMatrix(prev_x_[t], prev_u_[t]);
+      Eigen::Matrix<double, 3, 2> B = computeBMatrix(prev_x_[t]);
+      auto K = (R_ + B.transpose() * last_S * B).inverse() * B.transpose() * last_S * A;
+      S_[t] = A.transpose() * last_S * A - (A.transpose() * last_S * B) * K + Q_;
+    }
+
+    // computes the forward pass to update the states and controls
+    Eigen::Vector3d cur_x = init_x;
+    for(int t = 0; t < T_/dt_; t++) {
+      Eigen::Matrix3d A = computeAMatrix(cur_x, prev_u_[t]);
+      Eigen::Matrix<double, 3, 2> B = computeBMatrix(prev_x_[t]);
+      auto current_S = S_[t];
+      auto K = (R_ + B.transpose() * current_S * B).inverse() * B.transpose() * current_S * A;
+      Eigen::Vector2d u_star = -K * (cur_x - trajectory_[t]);
+
+      prev_x_[t] = cur_x;
+      prev_u_[t] = u_star;
+
+      cur_x = computeNextState(cur_x, u_star);
+    }
+  }
+
 private:
   rclcpp_lifecycle::LifecycleNode::SharedPtr node_;
+  // dynamics
+  double dt_ = 0.1;
+  double T_ = 1;
+
+  // cost function
+  Eigen::Matrix3d Q_ = Eigen::Matrix3d::Identity();
+  Eigen::Matrix3d Qf_ = Eigen::Matrix3d::Identity();
+  Eigen::Matrix2d R_ = Eigen::Matrix2d::Identity()*0.01;
+
+  // Ricatti equation
+  std::vector<Eigen::Matrix3d> S_;
+  std::vector<Eigen::Vector3d> prev_x_;
+  std::vector<Eigen::Vector2d> prev_u_;
+
+  // trajectory to track
   std::vector<Eigen::Vector3d> trajectory_;
 
 };
--- projects/week_7/Instructions.md
+++ projects/week_7/Instructions.md
@@ -6,7 +6,7 @@ We strongly recommend viewing this file with a rendered markdown viewer. You can
  - Opening this file in any other markdown viewer you prefer
 -->
 
-# Week 5 Project: Mapping
+# Week 7 Project: Linear Quadratic Regulator
 
 <!-- START doctoc generated TOC please keep comment here to allow auto update -->
 <!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
@@ -20,6 +20,53 @@ We strongly recommend viewing this file with a rendered markdown viewer. You can
 
 ## 1 Background
 
+In this project we will be writing an implementation of LQR.
+Our robot has a nominal path that we will need to follow through the world.
+
+In the final challenge for this semester, our robot will need to use every other project to complete the entire course.
+The key part of that for this week is following a trajectory.
+
+
 ## 2 Running this project
 
+To run this week's project, you'll need to run one launch file:
+
+```bash
+$ ros2 launch rj_training_bringup week_7.launch.xml
+```
+
+As usual, this launch file will startup the simulator, rviz, and all of the other nodes the project requires.
+
+By default, this will use the particle filter localizer node you worked on in project 3. You can optionally use the fake localizer with the `use_fake_localizer` argument.
+
+```bash
+$ ros2 launch rj_training_bringup week_7.launch.xml use_fake_localizer:=true
+```
+
 ## 3 Instructions
+
+### 3.1 Get the latest starter code
+
+To make sure you're starting with the latest starter code, pull from the git server in your copy of the software-training repository.
+
+```bash
+$ cd training_ws/src/software-training
+$ git pull
+```
+
+You'll also want to make sure you've got the latest version of the training support library by running an apt package update.
+
+```bash
+$ sudo apt update
+$ sudo apt upgrade
+```
+
+If you have done a different installation of stsl that is not through apt make sure to pull the latest code there.
+
+### 3.10 Commit your new code in git
+
+Once you've got your code for this project working, use the command below to commit it into git. This will make it easier to grab changes to the starter code for the remaining projects.
+
+```bash
+$ git commit -a -m "My project 6 code."
+```
