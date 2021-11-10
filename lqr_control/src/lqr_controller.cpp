08ec6a4ebd4f830ebb952270cfbf064227f78660
Jason Gibson <jgibson37@gatech.edu>
2021-11-09 21:40:08 -0500

    fixed linter errors on base code

--- lqr_control/src/lqr_controller.cpp
+++ lqr_control/src/lqr_controller.cpp
@@ -23,55 +23,71 @@
 #include <tf2_eigen/tf2_eigen.h>
 #include <Eigen/Dense>
 #include <angles/angles.h>
+#include <vector>
+#include <memory>
+#include <string>
+#include <algorithm>
 
 namespace lqr_control
 {
 
-Eigen::Vector3d StateFromMsg(const geometry_msgs::msg::PoseStamped& pose) {
+Eigen::Vector3d StateFromMsg(const geometry_msgs::msg::PoseStamped & pose)
+{
   Eigen::Quaterniond orientation;
   tf2::fromMsg(pose.pose.orientation, orientation);
   Eigen::Vector3d state;
-  state << pose.pose.position.x, pose.pose.position.y, orientation.toRotationMatrix().eulerAngles(0, 1, 2)[2];
+  state << pose.pose.position.x, pose.pose.position.y,
+    orientation.toRotationMatrix().eulerAngles(0, 1, 2)[2];
   return state;
 }
 
 class LqrController : public nav2_core::Controller
 {
 public:
-  void configure(const rclcpp_lifecycle::LifecycleNode::SharedPtr & node, std::string name, const std::shared_ptr<tf2_ros::Buffer> & tf_buffer, const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap) override {
+  void configure(
+    const rclcpp_lifecycle::LifecycleNode::SharedPtr & node,
+    std::string name,
+    const std::shared_ptr<tf2_ros::Buffer> & tf_buffer,
+    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> & costmap) override
+  {
     // BEGIN STUDENT CODE
     // END STUDENT CODE
   }
 
-  Eigen::Vector3d interpolateState(const rclcpp::Time time) {
-    if (time.seconds() > path_start_time_.seconds()+(time_between_states_*trajectory_.size())) {
+  Eigen::Vector3d interpolateState(const rclcpp::Time time)
+  {
+    if (time.seconds() > path_start_time_.seconds() + (time_between_states_ * trajectory_.size())) {
       return trajectory_.back();
     }
-    if(time < path_start_time_) {
+    if (time < path_start_time_) {
       return trajectory_.front();
     }
 
     double rel_time = (time - path_start_time_).seconds();
-    int lower_idx = (int) (rel_time / time_between_states_);
-    int upper_idx = lower_idx+1;
+    int lower_idx = std::floor(rel_time / time_between_states_);
+    int upper_idx = lower_idx + 1;
     double alpha = (rel_time - lower_idx * time_between_states_) / time_between_states_;
 
-    Eigen::Vector3d interpolated = (1-alpha)*trajectory_[lower_idx] + alpha*trajectory_[upper_idx];
+    Eigen::Vector3d interpolated = (1 - alpha) * trajectory_[lower_idx] + alpha *
+      trajectory_[upper_idx];
 
     // correct the angle average
-    // https://www.ri.cmu.edu/pub_files/pub4/kuffner_james_2004_1/kuffner_james_2004_1.pdf algorithm 7
-    if ((trajectory_[lower_idx](2) > 0 && trajectory_[upper_idx](2) < 0
-         || trajectory_[lower_idx](2) < 0 && trajectory_[upper_idx](2) > 0)
-        && (std::abs(trajectory_[lower_idx](2)) > M_PI_2 && std::abs(trajectory_[upper_idx](2)) > M_PI_2)) {
-      double angle_diff = angles::shortest_angular_distance(trajectory_[lower_idx](2), trajectory_[upper_idx](2));
-      interpolated(2) = trajectory_[lower_idx](2) + alpha*angle_diff;
+    if ((trajectory_[lower_idx](2) > 0 && trajectory_[upper_idx](2) < 0 ||
+      trajectory_[lower_idx](2) < 0 && trajectory_[upper_idx](2) > 0) &&
+      (std::abs(trajectory_[lower_idx](2)) > M_PI_2 && std::abs(
+        trajectory_[upper_idx](2)) > M_PI_2))
+    {
+      double angle_diff = angles::shortest_angular_distance(
+        trajectory_[lower_idx](2), trajectory_[upper_idx](2));
+      interpolated(2) = trajectory_[lower_idx](2) + alpha * angle_diff;
       interpolated(2) = angles::normalize_angle(interpolated(2));
     }
 
     return interpolated;
   }
 
-  void activate() override {
+  void activate() override
+  {
     // BEGIN STUDENT CODE
     // END STUDENT CODE
   }
@@ -80,17 +96,22 @@ public:
 
   void cleanup() override {}
 
-  void setPlan(const nav_msgs::msg::Path & path) override {
+  void setPlan(const nav_msgs::msg::Path & path) override
+  {
     trajectory_.clear();
-    std::transform(path.poses.begin(), path.poses.end(), std::back_inserter(trajectory_), StateFromMsg);
+    std::transform(
+      path.poses.begin(), path.poses.end(),
+      std::back_inserter(trajectory_), StateFromMsg);
     path_start_time_ = node_->now();
   }
 
-  geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped & pose, const geometry_msgs::msg::Twist & velocity) override
+  geometry_msgs::msg::TwistStamped computeVelocityCommands(
+    const geometry_msgs::msg::PoseStamped & pose,
+    const geometry_msgs::msg::Twist & velocity) override
   {
     Eigen::Vector3d state = StateFromMsg(pose);
 
-    for(int i = 0;i < iterations_; i++) {
+    for (int i = 0; i < iterations_; i++) {
       computeRicattiEquation();
       computeForwardPass(state, pose.header.stamp);
     }
@@ -103,29 +124,33 @@ public:
     return cmd_vel_msg;
   }
 
-  Eigen::Matrix3d computeAMatrix(const Eigen::Vector3d& x, const Eigen::Vector2d& u) {
+  Eigen::Matrix3d computeAMatrix(const Eigen::Vector3d & x, const Eigen::Vector2d & u)
+  {
     // BEGIN STUDENT CODE
     return Eigen::Matrix3d::Zero();
     // END STUDENT CODE
   }
 
-  Eigen::Matrix<double, 3, 2> computeBMatrix(const Eigen::Vector3d& x) {
+  Eigen::Matrix<double, 3, 2> computeBMatrix(const Eigen::Vector3d & x)
+  {
     // BEGIN STUDENT CODE
     return Eigen::Matrix<double, 3, 2>::Zero();
     // END STUDENT CODE
   }
 
-  Eigen::Vector3d computeNextState(const Eigen::Vector3d x, const Eigen::Vector2d u) {
+  Eigen::Vector3d computeNextState(const Eigen::Vector3d x, const Eigen::Vector2d u)
+  {
     // BEGIN STUDENT CODE
     return x;
     // END STUDENT CODE
   }
 
-  void computeRicattiEquation() {
+  void computeRicattiEquation()
+  {
     // does the backwards pass of the ricatti equation
-    S_[S_.size()-1] = Qf_;
-    for(int t = T_/dt_ - 2; t >= 0; t--) {
-      auto last_S = S_[t+1];
+    S_[S_.size() - 1] = Qf_;
+    for (int t = T_ / dt_ - 2; t >= 0; t--) {
+      auto last_S = S_[t + 1];
       Eigen::Matrix3d A = computeAMatrix(prev_x_[t], prev_u_[t]);
       Eigen::Matrix<double, 3, 2> B = computeBMatrix(prev_x_[t]);
       auto K = (R_ + B.transpose() * last_S * B).inverse() * B.transpose() * last_S * A;
@@ -133,15 +158,17 @@ public:
     }
   }
 
-  void computeForwardPass(const Eigen::Vector3d& init_x, rclcpp::Time current_time) {
+  void computeForwardPass(const Eigen::Vector3d & init_x, rclcpp::Time current_time)
+  {
     // computes the forward pass to update the states and controls
     Eigen::Vector3d cur_x = init_x;
-    for(int t = 0; t < T_/dt_; t++) {
+    for (int t = 0; t < T_ / dt_; t++) {
       Eigen::Matrix3d A = computeAMatrix(cur_x, prev_u_[t]);
       Eigen::Matrix<double, 3, 2> B = computeBMatrix(prev_x_[t]);
       auto current_S = S_[t];
       auto K = (R_ + B.transpose() * current_S * B).inverse() * B.transpose() * current_S * A;
-      Eigen::Vector3d state_error = (cur_x - interpolateState(current_time + rclcpp::Duration(dt_*t*1e9)));
+      Eigen::Vector3d state_error = (cur_x -
+        interpolateState(current_time + rclcpp::Duration(dt_ * t * 1e9)));
       state_error(2) = angles::normalize_angle(state_error(2));
       Eigen::Vector2d u_star = -K * state_error;
 
@@ -154,7 +181,7 @@ public:
     nav_msgs::msg::Path path;
     path.header.stamp = current_time;
     path.header.frame_id = "/map";
-    for(const Eigen::Vector3d current_state : prev_x_) {
+    for (const Eigen::Vector3d current_state : prev_x_) {
       geometry_msgs::msg::PoseStamped pose;
       pose.pose.position.x = current_state(0);
       pose.pose.position.y = current_state(1);
@@ -184,8 +211,8 @@ private:
   std::vector<Eigen::Vector3d> trajectory_;
   double time_between_states_;
   rclcpp::Time path_start_time_;
-  rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>::SharedPtr traj_viz_pub_;
-
+  rclcpp_lifecycle::LifecyclePublisher
+  <nav_msgs::msg::Path>::SharedPtr traj_viz_pub_;
 };
 
 }  // namespace lqr_control
--- lqr_control/src/test_path_generator.cpp
+++ lqr_control/src/test_path_generator.cpp
@@ -1,7 +1,27 @@
+// Copyright 2021 RoboJackets
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #include "test_path_generator.hpp"
-#include <cmath>
-#include <tf2_eigen/tf2_eigen.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <cmath>
 
 namespace lqr_control
 {
@@ -33,9 +53,11 @@ geometry_msgs::msg::PoseStamped TestPathGenerator::GetNextPoint()
   const auto dx = scale_ * std::cos(t_);
   const auto dy = (scale_ * std::cos(t_) * std::cos(t_)) - (scale_ * std::sin(t_) * std::sin(t_));
 
-  pose.pose.orientation = tf2::toMsg(Eigen::Quaterniond{Eigen::AngleAxisd{std::atan2(dy,
+  pose.pose.orientation = tf2::toMsg(
+    Eigen::Quaterniond{Eigen::AngleAxisd{std::atan2(
+          dy,
           dx),
-          Eigen::Vector3d::UnitZ()}});
+        Eigen::Vector3d::UnitZ()}});
 
   t_ += t_delta_;
   return pose;
