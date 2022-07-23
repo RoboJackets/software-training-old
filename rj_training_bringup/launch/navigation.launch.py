0f805bfe9d196576d7f2001babac6df885dbf48d
Matthew Barulic <matthew.barulic@gmail.com>
2022-07-22 23:50:32 -0400

    Removes unused import

--- rj_training_bringup/launch/navigation.launch.py
+++ rj_training_bringup/launch/navigation.launch.py
@@ -20,7 +20,7 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
